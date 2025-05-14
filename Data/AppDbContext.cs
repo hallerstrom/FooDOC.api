37fb916db53ee31f4d9cd7bece23b4d876e01adc
Linus Hallerström <hallerstrom.linus@gmail.com>
2025-05-14 11:20:04 +0200

    CCP endpoints

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,20 +1,69 @@
 using System;
 using FooDOC.api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FooDOC.api.Data;
 
-public class AppDbContext : IdentityDbContext 
+public class AppDbContext : IdentityDbContext<IdentityUser>
 {
+    public DbSet<TempCCP> TempCCPs { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         this.Database.EnsureCreated();
     }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Skapa en hasher-instans
+        var hasher = new PasswordHasher<IdentityUser>(); 
+
+        // Skapa admin-användaren
+        var adminUser = new IdentityUser
         {
-        base.OnModelCreating(builder);
-        }
+            Id = "1", 
+            UserName = "admin",
+            NormalizedUserName = "ADMIN",
+            Email = "admin@example.com",
+            NormalizedEmail = "ADMIN@EXAMPLE.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString(),
+        };
+        adminUser.PasswordHash = hasher.HashPassword(adminUser, "Admin123!");
+
+        modelBuilder.Entity<IdentityUser>().HasData(adminUser);
+
+        modelBuilder.Entity<IdentityUserClaim<string>>().HasData(new IdentityUserClaim<string>
+        {
+        Id = 1,
+        UserId = "1",
+        ClaimType = "role",
+        ClaimValue = "admin"
+        });
+
+        // Skapa user-användaren
+        var userUser = new IdentityUser 
+        {
+            Id = "2", 
+            UserName = "user",
+            NormalizedUserName = "USER",
+            Email = "user@example.com",
+            NormalizedEmail = "USER@EXAMPLE.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString(),
+        };
+        userUser.PasswordHash = hasher.HashPassword(userUser, "User123!");
+
+        modelBuilder.Entity<IdentityUser>().HasData(userUser); 
+
+        // Seeda kontrollpunkter som mockdata
+        modelBuilder.Entity<TempCCP>().HasData(
+            new TempCCP { Id = 1, Product = "Hel Kyckling", Temp = 99.54, CreatedAt = DateTime.UtcNow },
+            new TempCCP { Id = 2, Product = "Kamben", Temp = 30.55, CreatedAt = DateTime.UtcNow }
+        );
+    }
 }
 
